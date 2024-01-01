5214dd4a14af0d0e6694382c0da825e6376b1463
minhvuong123 <minhvuong12222@gmail.com>
2024-01-02 00:24:27 +0700

    add search bar

--- app/components/nav/SearchBar.tsx
+++ app/components/nav/SearchBar.tsx
@@ -1,6 +1,42 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import queryString from "query-string";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
 const SearchBar = () => {
+  const router = useRouter();
+  const {register, handleSubmit, reset, formState: { errors }} = useForm<FieldValues>({
+    defaultValues: {
+      searchTerm: ''
+    }
+  })
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    if (!data.searchTerm) {
+      return router.push('/')
+    }
+
+    const url = queryString.stringifyUrl({
+      url: '/',
+      query: {
+        searchTerm: data.searchTerm
+      }
+    }, { skipNull: true })
+
+    router.push(url)
+    reset();
+  }
+
   return (
-    <div></div>
+    <div className="flex items-center">
+      <input 
+        {...register('searchTerm')}
+        type="text"
+        autoComplete="off"
+        className="p-2 border border-gray-300 rounded-1-md focus:outline-none focus:border-[0.5px] focus:border-slate-500 w-80" placeholder="Explore Ecom-shop" />
+        <button onClick={handleSubmit(onSubmit)} className="bg-slate-700 hover:opacity-80 text-white p-2 rounded-r-md">Search</button>
+    </div>
   )
 }
 
